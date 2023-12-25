cdb96b7e41f23f92404a495f87796160d8f4ef23
Pichau <ademirzinhoperes@gmail.com>
2023-12-25 09:03:27 -0300

    enviando projeto

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,50 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>foto
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-fab vertical="buttom" horizontal="center" slot="fixed">
+
+        <ion-fab-button @click="takePhoto()">
+
+
+      <ion-tab-button tab="tab2" href="/tabs/tab2">
+        <ion-icon :icon="camera" />
+      </ion-tab-button>
+
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+
+      <!--<ExploreContainer name="Tab 1 page" />-->
+
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { images, square, triangle } from 'ionicons/icons';
+import {
+  //IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
+import { camera, trash, close } from 'ionicons/icons';
+import { usePhotoGallery } from '../components/composables/usePhotoGalerry';
+
+const { takePhoto } = usePhotoGallery();
+
+//import ExploreContainer from '@/components/ExploreContainer.vue';
+</script>
